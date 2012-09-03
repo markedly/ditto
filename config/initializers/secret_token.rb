504fae3845e2d9c1a198dad4b6f946d7fd5b9143
mark <markhinds@gmail.com>
2012-09-03 02:02:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ditto::Application.config.secret_token = '2dfae79d6a0cbad6e1935f3320d79fcb2031b7b878f0824dac96a00abf3ce5da882e2400ff23f69417fb6277357173a7db2c822857e38fa74dba11eb3308a443'
